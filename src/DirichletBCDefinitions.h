ee658148dc95b155939bae6fd19ad7cc318108c0
Massimiliano Leoni <leoni.massimiliano1@gmail.com>
2022-09-15 15:42:24 +0200

    Syncing with upstream API changes

--- src/DirichletBCDefinitions.h
+++ src/DirichletBCDefinitions.h
@@ -4,9 +4,14 @@
 #include <dolfinx/la/petsc.h>
 #include <dolfinx/mesh/MeshTags.h>
 
-using DirichletBCFunction_ret = xt::xarray<PetscScalar>;
+using DirichletBCFunction_ret =
+        std::pair<std::vector<PetscScalar>, std::vector<std::size_t>>;
 using DirichletBCFunction = const std::function<DirichletBCFunction_ret(
-        const xt::xtensor<double, 2> &)>;
+        std::experimental::mdspan<const double,
+                                  std::experimental::extents<
+                                          std::size_t,
+                                          3,
+                                          std::experimental::dynamic_extent>>)>;
 
 using DirichletBCList_key_type =
         std::pair<const dolfinx::mesh::MeshTags<int> &, int>;
