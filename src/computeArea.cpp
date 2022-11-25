944504bb281d3b15d118adfe99c2f47810c5cf8f
Massimiliano Leoni <leoni.massimiliano1@gmail.com>
2022-11-25 14:51:21 +0100

    Sync with upstream API

--- src/computeArea.cpp
+++ src/computeArea.cpp
@@ -10,12 +10,16 @@ PetscReal computeArea(const dolfinx::mesh::MeshTags<int>& tags, const int index)
 {
     std::vector<int> values(tags.values().size());
 
-    std::transform(tags.values().cbegin(),
-                   tags.values().cend(),
+    std::transform(tags.values().begin(),
+                   tags.values().end(),
                    values.begin(),
                    [=](const auto& val) { return val == index ? 0 : 1; });
-    auto integrationTags =
-            mesh::MeshTags<int>(tags.mesh(), 2, tags.indices(), values);
+    auto integrationTags = mesh::MeshTags<int>(
+            tags.mesh(),
+            2,
+            std::vector<std::int32_t>(tags.indices().begin(),
+                                      tags.indices().end()),
+            values);
 
     auto a = std::make_shared<fem::Constant<PetscScalar>>(1);
     auto M_area = std::make_shared<fem::Form<PetscScalar>>(
